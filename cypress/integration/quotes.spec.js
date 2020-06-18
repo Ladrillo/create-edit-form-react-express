b9621d9f727e5499079ab81375efa2b99e2d1af3
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-06-18 15:17:42 +0200

    add a few tests

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,91 +1,81 @@
-context('Friends Form Inputs', () => {
+describe('Quotes Form Inputs', () => {
   it('can navigate to the site', () => {
     cy.visit('http://localhost:1234')
     cy.url().should('include', 'localhost')
   })
 
-  it('button is disabled', () => {
-    cy.get('button.submit')
+  it('submit button is disabled', () => {
+    cy.get('button#submitBtn')
       .should('be.disabled')
   })
 
-  it('can type a username', () => {
-    cy.get('input[name="username"]')
-      .type('Lady Gaga')
-      .should('have.value', 'Lady Gaga')
+  it('can type a text for a new quote', () => {
+    cy.get('input[name="text"]')
+      .type('Have fun!')
+      .should('have.value', 'Have fun!')
   })
 
-  it('can type an email', () => {
-    cy.get('input[name="email"]')
-      .type('lady@gaga.com')
-      .should('have.value', 'lady@gaga.com')
-  })
-
-  it('can select a role', () => {
-    cy.get('select[name="role"]')
-      .select('Instructor')
-      .should('have.value', 'Instructor')
+  it('can type an author for a new quote', () => {
+    cy.get('input[name="author"]')
+      .type('Gabe')
+      .should('have.value', 'Gabe')
   })
 
   it('button is not disabled any more', () => {
-    cy.get('button.submit')
+    cy.get('button#submitBtn')
       .should('not.be.disabled')
   })
-})
 
-context('Username Validation Errors', () => {
-  it('can navigate to the site', () => {
-    cy.visit('http://localhost:1234')
+  it('can cancel the new quote', () => {
+    cy.get('button#cancelBtn').click()
   })
 
-  it('should not show validation errors from the get-go', () => {
-    cy.contains('Username required').should('not.exist')
+  it('text input is back to blank', () => {
+    cy.get('input[name="text"]')
+      .should('have.value', '')
   })
 
-  it('shows validation error for less than 3 chars', () => {
-    cy.get('input[name="username"]').type('a')
-    cy.contains('Username required')
-
-    cy.get('input[name="username"]').type('b')
-    cy.contains('Username required')
+  it('author input is back to blank', () => {
+    cy.get('input[name="author"]')
+      .should('have.value', '')
   })
 
-  it('validation error goes away for three chars', () => {
-    cy.get('input[name="username"]').type('c')
-    cy.contains('Username required').should('not.exist')
+  it('button is back to being disabled', () => {
+    cy.get('button#submitBtn')
+      .should('be.disabled')
   })
 })
 
-context('Creating and deleting a new friend', () => {
-  it('can navigate to the site', () => {
-    cy.visit('http://localhost:1234')
-  })
+// context('Creating and deleting a new friend', () => {
+//   it('can navigate to the site', () => {
+//     cy.visit('http://localhost:1234')
+//   })
 
-  it('can create a new friend', () => {
-    cy.get('input[name="username"]')
-      .type('Lady Gaga')
+//   it('can create a new friend', () => {
+//     cy.get('input[name="username"]')
+//       .type('Lady Gaga')
 
-    cy.get('input[name="email"]')
-      .type('lady@gaga.com')
+//     cy.get('input[name="email"]')
+//       .type('lady@gaga.com')
 
-    cy.get('select[name="role"]')
-      .select('Instructor')
-    
-    cy.get('button.submit').click()
+//     cy.get('select[name="role"]')
+//       .select('Instructor')
 
-    cy.get('.friend h2').contains('Lady Gaga')
-    cy.get('.friend p').contains('Email: lady@gaga.com')
-    cy.get('.friend p').contains('Role: Instructor')
-  })
+//     cy.get('button.submit').click()
 
-  it('can delete the new Friend', () => {
-    cy.get('.friend p').contains('Email: lady@gaga.com').next().next().click()
-    cy.get('.friend h2').contains('Lady Gaga').should('not.exist')
-    cy.get('.friend p').contains('Email: lady@gaga.com').should('not.exist')
-  })
+//     cy.get('.friend h2').contains('Lady Gaga')
+//     cy.get('.friend p').contains('Email: lady@gaga.com')
+//     cy.get('.friend p').contains('Role: Instructor')
+//   })
 
-  it('can delete the existing Friend', () => {
-    cy.get('.friend h2').contains('Michael').next().next().next().click()
-    cy.contains('Michael').should('not.exist')
-  })
-})
+//   it('can delete the new Friend', () => {
+//     cy.get('.friend p').contains('Email: lady@gaga.com').next().next().click()
+//     cy.get('.friend h2').contains('Lady Gaga').should('not.exist')
+//     cy.get('.friend p').contains('Email: lady@gaga.com').should('not.exist')
+//   })
+
+//   it('can delete the existing Friend', () => {
+//     cy.get('.friend h2').contains('Michael').next().next().next().click()
+//     cy.contains('Michael').should('not.exist')
+//   })
+// })
